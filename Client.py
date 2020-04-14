f30b1fb3612e290da1ecb85817d17b0d9fb9faff
haoranyu <hy47@cs.washington.edu>
2020-04-13 22:42:39 -0700

    Finished part a

--- Client.py
+++ Client.py
@@ -1,7 +1,63 @@
 #!/usr/bin/env python3
 import socket
 
+
+def create(content, secret):
+    pad = int((len(content) + 3) / 4) * 4 - len(content)
+    return len(content).to_bytes(4, 'big') + secret.to_bytes(4, 'big') + \
+           (1).to_bytes(2, 'big') + (40).to_bytes(2, 'big') + content + bytes(pad)
+
+
+def check_header(content, secret):
+    pre = int.from_bytes(content[4:8], 'big')
+    step = int.from_bytes(content[8:10], 'big')
+    digit = int.from_bytes(content[10:12], 'big')
+    if pre != secret or step != 2 or digit != 40:
+        print('Illegal header')
+
+
+buffer = 1024
+ip = socket.gethostbyname("attu2.cs.washington.edu")
+port = 12235
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto("Hello, server".encode(), ('localhost', 9527))
-message, address = client.recvfrom(1024)
-print("recvData:", message.decode())
+client.sendto(create("hello world\0".encode(), 0), (ip, port))
+message, addr = client.recvfrom(buffer)
+check_header(message, 0)
+num = int.from_bytes(message[12:16], 'big')
+length = int.from_bytes(message[16:20], 'big')
+port = int.from_bytes(message[20:24], 'big')
+secretA = int.from_bytes(message[24:28], 'big')
+sendNum = 0
+client.settimeout(0.5)
+while sendNum < num:
+    send = create(sendNum.to_bytes(4, 'big') + bytes(length), secretA)
+    client.sendto(send, (ip, port))
+    try:
+        message, addr = client.recvfrom(buffer)
+        # may also need check
+        # print(message)
+        sendNum += 1
+    except socket.timeout as e:
+        continue
+message, addr = client.recvfrom(buffer)
+check_header(message, secretA)
+port = int.from_bytes(message[12:16], 'big')
+secretB = int.from_bytes(message[16:20], 'big')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip, port))
+message = client.recv(1024)
+check_header(message, secretB)
+num = int.from_bytes(message[12:16], 'big')
+length = int.from_bytes(message[16:20], 'big')
+secretC = int.from_bytes(message[20:24], 'big')
+c = message[24:25]
+for i in range(num):
+    s = b''
+    for j in range(length):
+        s += c
+    client.send(create(s, secretC))
+message = client.recv(buffer)
+check_header(message, secretC)
+secretD = int.from_bytes(message[12:16], 'big')
+client.close()
+print('A', secretA, 'B', secretB, 'C', secretC, 'D', secretD)
